cf1b30044115d5c7b9177e7282b976a76dbd883c
pmpellegrini7 <pmpellegrini7@gmail.com>
2021-05-01 04:30:52 -0300

    Prueba commit

---
+++ mi-ram-hq/src/tools.c
@@ -0,0 +1,10 @@
+#include <../include/tools.h>
+
+#define BACKLOG 10
+
+int iniciar_server(void){
+
+	printf("Hello world....");
+
+	return 0;
+}
---
+++ shared/main.c
@@ -0,0 +1,8 @@
+#include <./include/tools.h>
+
+int main(){
+
+	int prueba = iniciar_server();
+
+	return 0;
+}
