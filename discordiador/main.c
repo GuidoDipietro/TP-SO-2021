f7b986c318e65eb4ea9eeae7362d71a4dd77289a
GuidoDipietro <dipietroguido@gmail.com>
2021-05-02 18:49:33 -0300

    pruebas de cliente en discordiador (Cambiar)

--- discordiador/main.c
+++ discordiador/main.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+#include <commons/config.h>
+
+#include "../shared/include/sockets.h"
 #include "./include/console.h"
 
+#define MODULENAME "DIS"
+
 int main() {
+
+	// main del discordiador
+
+	////////////
+	// prueba cliente //
+
+	t_config* config = config_create("discordiador.config");
+
+	t_log* logger = log_create("log.log", MODULENAME, 1, LOG_LEVEL_DEBUG);
+
+	int conexion = crear_conexion(
+			logger,
+			"IMS",
+			config_get_string_value(config,"IP_IMS"),
+			config_get_string_value(config, "PUERTO_IMS")
+	); // eclipse pinta cosas grises aca porque es una ******
+
+	////////////
+
 	menu_start();
-	return 0;
+
+	log_info(logger, "Saliendo de DIS");
+	//frees
+	config_destroy(config);
+	log_destroy(logger);
+	liberar_conexion(&conexion);
+
+	return EXIT_SUCCESS;
 }
