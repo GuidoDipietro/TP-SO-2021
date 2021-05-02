36b2f6958929fb8c18697f4def5c8c297dd3b5bb
GuidoDipietro <dipietroguido@gmail.com>
2021-05-02 18:49:55 -0300

    makefile, main.c en mi-ram-hq

---
+++ mi-ram-hq/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+#include <commons/config.h>
+
+#include "../shared/include/sockets.h"
+
+#define MODULENAME "MRH"
+
+int main() {
+
+	// main del mi-ram-hq
+
+	return EXIT_SUCCESS;
+}
--- mi-ram-hq/src/tools.c
+++ mi-ram-hq/src/tools.c
@@ -1,4 +1,4 @@
-#include <../include/tools.h>
+#include "../include/tools.h"
 
 #define BACKLOG 10
 
