8d3aace6821b9a6d0dfd3bf9e375718fc8745a04
Guido Dipietro <dipietroguido@gmail.com>
2021-05-17 19:08:11 -0300

    reorganizando headers de mi-ram-hq/main.c + logger global

---
+++ mi-ram-hq/include/main.h
@@ -0,0 +1,30 @@
+#ifndef MI_RAM_HQ_MAIN_H
+#define MI_RAM_HQ_MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <curses.h>
+
+t_log* logger;
+
+#include "graphic.h"
+#include "init_mrhq.h"
+#include "../../shared/include/utils.h"
+#include "../../shared/include/sockets.h"
+#include "../../shared/include/protocolo.h"
+
+#define MODULENAME "MRH"
+#define SERVERNAME "MRH_SERVER"
+#define NIVELNAME "AMONG-OS"
+
+static t_config_mrhq* initialize_cfg() {
+    t_config_mrhq* cfg = malloc(sizeof(t_config_mrhq));
+    cfg->ALGORITMO_REEMPLAZO = NULL;
+    cfg->ESQUEMA_MEMORIA = NULL;
+    cfg->PATH_SWAP = NULL;
+    return cfg;
+}
+
+#endif
--- mi-ram-hq/main.c
+++ mi-ram-hq/main.c
@@ -1,29 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <commons/log.h>
-#include <commons/config.h>
-#include "../shared/include/utils.h"
-#include "../shared/include/sockets.h"
-#include "include/graphic.h"
-#include "include/init_mrhq.h"
-#include "../shared/include/protocolo.h"
-#include <curses.h>
-
-#define MODULENAME "MRH"
-#define SERVERNAME "MRH_SERVER"
-#define NIVELNAME "AMONG-OS"
-
-static t_config_mrhq* initialize_cfg() {
-    t_config_mrhq* cfg = malloc(sizeof(t_config_mrhq));
-    cfg->ALGORITMO_REEMPLAZO = NULL;
-    cfg->ESQUEMA_MEMORIA = NULL;
-    cfg->PATH_SWAP = NULL;
-    return cfg;
-}
+#include "include/main.h"
 
 int main() {
     t_config_mrhq* cfg = initialize_cfg();
-    t_log* logger = log_create("mi-ram-hq.log", MODULENAME, true, LOG_LEVEL_INFO);
+    logger = log_create("mi-ram-hq.log", MODULENAME, true, LOG_LEVEL_INFO);
 
     if(!cargar_configuracion(cfg, logger)) {
         cerrar_programa(cfg, logger);
