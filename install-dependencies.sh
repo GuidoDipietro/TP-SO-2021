beb814e97e13f09a2324190f0c5224f3966e1a5d
crisszkutnik <crisszkutnik@gmail.com>
2021-07-17 02:11:18 -0300

    Script y cambios para Guido

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,17 +1,24 @@
+#sudo apt-get install -y libncurses5-dev
+original=$(pwd)
+echo $original
+
+# Instalamos las commons
+
 cd ~
 mkdir so-libraries
 cd so-libraries
 
+git clone https://github.com/sisoputnfrba/so-commons-library.git
+cd so-commons-library
+sudo make install
+cd ..
+
 # Instalamos la libreria de gui
 
-sudo apt-get install -y libncurses5-dev
 git clone https://github.com/sisoputnfrba/so-nivel-gui-library/
 cd so-nivel-gui-library
 make install
 cd ..
 
-# Instalamos las commons
-
-git clone https://github.com/sisoputnfrba/so-commons-library.git
-cd so-commons-library
-sudo make install
+cd $original
+make build
